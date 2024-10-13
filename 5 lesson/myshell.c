62dfe072e5878583490a1273bbb1b559b765a3be
OlegOpyakin <145427708+OlegOpyakin@users.noreply.github.com>
2024-10-13 19:38:25 +0300

    Parser finished
    Parser uses strtok to parse the line, yet it doesnt save it anywhere

--- 5 lesson/myshell.c
+++ 5 lesson/myshell.c
@@ -12,7 +12,35 @@ struct buffer{
     char* data_;
     size_t exit_flag_;
     size_t length_;
+    size_t progs_number_;
+    char*** progs_and_args_;
 };
+
+void LineParse(struct buffer* Buffer){
+    char *saveptr1, *saveptr2;
+    char *lexema1, *lexema2;
+    
+    while(1){
+        lexema1 = strtok_r(Buffer->data_, "|\n", &saveptr1);
+        if(lexema1 == NULL) 
+            break;
+
+        printf("lexema1: %s\n", lexema1);
+
+        while(1){
+            lexema2 = strtok_r(lexema1, " ", &saveptr2);
+
+            if(lexema2 == NULL) 
+                break;
+
+            printf("lexema2: %s\n", lexema2);
+            
+            lexema1 = NULL;
+        }
+
+        Buffer->data_ = NULL;
+    }
+}
 
 int main(){
     struct buffer Buffer;
@@ -23,15 +51,16 @@ int main(){
     while(Buffer.exit_flag_ == 0){
         write(1, "% ", 2);
         Buffer.length_ = getline(&Buffer.data_, &Buffer.size_, stdin);
+
         if(Buffer.length_ == -1){
             perror("Getline error");
             exit(1);
         }
 
-        if(strncmp(Buffer.data_, "q", 1) == 0){
+        if(strncmp(Buffer.data_, "exit", 4) == 0){
             break;
         }
-        //LineParse(&Buffer);
+        LineParse(&Buffer);
         //ExecFiles(&Buffer);
     }
     free(Buffer.data_);
